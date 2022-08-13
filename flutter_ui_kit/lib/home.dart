75d8cf9243da9adfe834234525217071d45dd7b4
Chandan Gowda <chandansuresh007@gmail.com>
2022-08-13 20:40:40 +0530

    improved UI

--- flutter_ui_kit/lib/home.dart
+++ flutter_ui_kit/lib/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_kit/coffee_app/screens/home_screen.dart';
 import 'package:flutter_ui_kit/crypto_app/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+List<Map<String, String>> AppPaths = [
+  {'name': "Coffee App", 'path': CoffeeAppHomeScreen.routeName},
+  {'name': "Crypto App", 'path': CryptoAppHomeScreen.routeName}
+];
 
-List<Map<String, String>> AppPaths=[{'name':"Coffee App",'path':CoffeeAppHomeScreen.routeName},{'name':"Crypto App",'path':CryptoAppHomeScreen.routeName}];
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -11,27 +15,39 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter UI Kit"),
+        title: Text("Flutter UI Kit",
+            style: GoogleFonts.raleway(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.cyan, Colors.lightBlueAccent])),
+        ),
       ),
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: ListView(
             children: AppPaths.map((screen) => GestureDetector(
-              onTap: (){
-                final String path = screen['path'] as String;
-                Navigator.pushNamed(context, path);
-                //Navigator.of(context).pushNamed(path);
-              },
-              child: Card(
-                elevation: 3.0,
-                shadowColor: Colors.black,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(screen['name'].toString(),style: TextStyle(fontSize: 20),),
-                ),
-              ),
-            )).toList(),
+                  onTap: () {
+                    final String path = screen['path'] as String;
+                    Navigator.pushNamed(context, path);
+                    //Navigator.of(context).pushNamed(path);
+                  },
+                  child: Card(
+                    elevation: 3.0,
+                    shadowColor: Colors.black,
+                    color: Colors.cyan.shade100,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                          child: Text(
+                        screen['name'].toString(),
+                        style: GoogleFonts.raleway(fontSize: 20),
+                      )),
+                    ),
+                  ),
+                )).toList(),
           ),
         ),
       ),
