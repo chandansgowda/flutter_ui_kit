c2e2b8bfe44869de62da8f80a717d5e0cc1a34bb
Chandan Gowda <chandansuresh007@gmail.com>
2022-08-16 19:31:52 +0530

    added search & categories structure

--- flutter_ui_kit/lib/furniture_app/screens/home_screen.dart
+++ flutter_ui_kit/lib/furniture_app/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_kit/furniture_app/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FurnitureAppHomeScreen extends StatelessWidget {
   static const routeName = "furniture-app";
@@ -8,16 +10,46 @@ class FurnitureAppHomeScreen extends StatelessWidget {
     return Theme(
       data: ThemeData(brightness: Brightness.light, primarySwatch: Colors.blue),
       child: Scaffold(
-        backgroundColor: Color.fromRGBO(4, 79, 157, 100),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Text("Dashboard"),
-                Icon(Icons.notifications_none_outlined),
-              ],
-            )
-          ],
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Dashboard", style: GoogleFonts.montserrat(color: Colors.white, fontSize: 25),),
+                    Icon(Icons.notifications_none_outlined, color: Colors.white,),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+                  prefixIcon: Icon(Icons.search_rounded),
+                    prefixIconColor: Colors.white,
+                    labelText: "Search",
+
+                ),
+              ),
+              Container(
+                height: 35,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.all(5),
+                  children: [
+                    Text("All"),
+                    Text("Sofa"),
+                    Text("Park bench"),
+                    Text("Armchair"),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
